4e3fc0e89703d68c287c3a17c5e69ff9f809cbd4
gemmaro <gemmaro.dev@gmail.com>
2024-11-05 22:33:12 +0900

    Add sample 2 test.

---
+++ test/sample2_test.rb
@@ -0,0 +1,12 @@
+require "test_helper"
+require "sample2"
+
+class Sample2Test < Test::Unit::TestCase
+  test "sample 2" do
+    parser = Sample2Parser.new
+    assert_equal 11, parser.parse("1+10")
+    assert_equal 0, parser.parse("2 - 1 * 20 + 18")
+    assert_equal 21, parser.parse("2 - (1 - 20)")
+    assert_equal 0, parser.parse("1 + 2 - 3")
+  end
+end
