fbabe3aef6d2c73431d9eb49f88c7ab8752c0285
gemmaro <gemmaro.dev@gmail.com>
2024-11-06 22:14:01 +0900

    Split grammar class.

--- lib/tdparser.rb
+++ lib/tdparser.rb
@@ -4,6 +4,8 @@
 # Top-down parser for embedded in a ruby script.
 #
 
+require "tdparser/grammar"
+
 module TDParser
   class TokenGenerator
     def initialize(args = nil, &block)
@@ -687,46 +689,6 @@ module TDParser
   end
 
   alias chain_right chainr
-
-  class Grammar
-    include TDParser
-
-    def define(&block)
-      instance_eval {
-        begin
-          alias method_missing g_method_missing
-          block.call(self)
-        end
-      }
-    end
-
-    def g_method_missing(sym, *args) # :nodoc:
-      arg0 = args[0]
-      sym = sym.to_s
-      if sym[-1, 1] == "="
-        name = sym[0..-2]
-        case arg0
-        when Parser
-          self.class.instance_eval {
-            method_defined?(name) or
-              define_method(name) { arg0 }
-          }
-        else
-          t = token(arg0)
-          self.class.instance_eval {
-            method_defined?(name) or
-            define_method(name) { t }
-          }
-        end
-      elsif args.size == 0
-        rule(sym)
-      else
-        raise(NoMethodError, "undefined method `#{sym}' for #{self.inspect}")
-      end
-    end
-
-    alias method_missing g_method_missing # :nodoc:
-  end
 
   def TDParser.define(*_args, &)
     klass = Class.new(Grammar)
