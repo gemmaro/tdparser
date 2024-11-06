cfb0fce9303d52cd128334ab4aaebb77e1353500
gemmaro <gemmaro.dev@gmail.com>
2024-11-06 23:22:14 +0900

    Refactoring grammar.

--- lib/tdparser/grammar.rb
+++ lib/tdparser/grammar.rb
@@ -1,19 +1,24 @@
+# frozen_string_literal: true
+
 module TDParser
   class Grammar
     include TDParser
 
     alias define instance_eval
 
+    def respond_to_missing? # :nodoc:
+      true
+    end
+
     def method_missing(sym, *args) # :nodoc:
       args.empty? and return rule(sym)
-      sym.end_with?('=') or raise(NoMethodError, "undefined method `#{sym}' for #{self.inspect}")
+      sym.end_with?('=') or raise(NoMethodError, "undefined method `#{sym}' for #{inspect}")
+      name = sym[0..-2]
+      klass = self.class
+      klass.method_defined?(name) and return
       parser, = args
       parser.is_a?(Parser) or parser = token(parser)
-      self.class.instance_eval {
-        name = sym[0..-2]
-        method_defined?(name) and return
-        define_method(name) { parser }
-      }
+      klass.define_method(name) { parser }
     end
   end
 end
